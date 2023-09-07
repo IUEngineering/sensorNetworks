738b0287e2aa18083c0db49797d342f820a8aba4
Jochem Leijenhorst <jochemleijenhorst@gmail.com>
2023-09-07 13:12:30 +0200

    Added commentage to  the code.

--- nrfTest/src/nrfTest.c
+++ nrfTest/src/nrfTest.c
@@ -27,6 +27,7 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include <string.h>
 #include <stdlib.h>
 #include "serialF0.h"
@@ -78,51 +79,70 @@ int main(void) {
     sei();
     clear_screen();
     
+    // Send welcome message
     printf("Welkom bij de nrftester\nGemaakt door Jochem Leijenhorst.\n\nTyp help voor een lijst met commando's.\n");
-    uint16_t channel = 54;
-    scanf("%d", &channel);
-    nrfInit(channel);
-    printf("Gestart met channel %d. Geen idee of dat legaal is, maar dat is jouw probleem.\n\n", channel);
+    for(uint8_t i = 0; i < 64; i++) uartF0_putc('-');
+    printf("\n\n");
+    nrfInit(54);
     
+    
+    // Make a buffer for the command.
+    // Not going to worry about buffer overflow, just don't input too much and you'll be fine.
     char inputBuffer[256];
+
+    // Make a char pointer to insert the next typed character into the buffer.
     char *inChar = inputBuffer;
 
     while (1) {
+        // Get the character from the user.
         char newInputChar = uartF0_getc();
         if(newInputChar != '\0') {
-            if(newInputChar == '\r') newInputChar = '\n';
+
+            // Backspace support :)
             if(newInputChar == '\b') {
                 if(inChar != inputBuffer) {
                     inChar--;
+                    // Go back one character, replace the next character with a space, and then go back one character again.
                     printf("\b \b");
                 }
                 continue;
             }
 
             *inChar = newInputChar;
-            uartF0_putc(newInputChar);
-            if(newInputChar == '\n') {
+
+            if(newInputChar == '\r') {
+                // Things like minicom and teraterm send return characters as \r. Very annoying.
                 *inChar = '\0';
-                uartF0_putc('\r');
+                printf("\n");
                 runCommand(inputBuffer);
                 inChar = inputBuffer;
             }
-            else inChar++;
+            else {
+                inChar++;
+                // Provide an echo of what the user is typing.
+                // Otherwise the user's input would be invisible to the user.
+                uartF0_putc(newInputChar);
+            }
         }
     }
 }
 
 
 void runCommand(char *command) {
-    void (*comFunc[COMMANDS])(char*) = {
+    // Make an array of functions.
+    const void (*comFunc[COMMANDS])(char*) = {
         wpip, rpip, send, help, chan
     };
-    char commands[COMMANDS][4] = {
+
+    // Make a corresponding array of 4 letter function names.
+    const char commands[COMMANDS][4] = {
         "wpip", "rpip", "send", "help", "chan"
     };
 
+    // Start comparing the sent command with the command names.
     for(uint8_t i = 0; i < COMMANDS; i++) {
         if(strncmp(commands[i], command, 4) == 0) {
+            // Run the command, with the text after it as argument.
             comFunc[i](command + 5);
             return;
         }
@@ -137,46 +157,53 @@ void wpip(char *command) {
 }
 
 void rpip(char *command) {
+    // Pipenames are max 5 characters + 1 null character.
     char pipeName[6];
     uint8_t pipeIndex = 0;
     char *spaceChar = strchr(command, ' ');
     if(spaceChar == NULL) spaceChar = strchr(command, '\0');
 
-    if(spaceChar != NULL || spaceChar - command > 5) {
-        strncpy(pipeName, command, spaceChar - command);
-        pipeName[spaceChar - command] = '\0';
+    // Check if there is even a pipename given, and if it's not longer than 5 characters.
+    if(spaceChar != NULL) {
+        uint8_t nameLength = spaceChar - command;
+        // Make sure to not overflow the array.
+        strncpy(pipeName, command, nameLength <= 5 ? nameLength : 5);
+        pipeName[nameLength] = '\0';
     }
     else {
-        printf("No pipename provided\n");
+        printf("No valid pipename provided\n");
         return;
     }
 
+    // Check if there is a second argument (the reading pipe index).
     spaceChar = strchr(command, ' ');
     if(spaceChar != NULL && spaceChar + 1 != NULL) pipeIndex = atoi(spaceChar + 1);
-     
+    
+    // Open the pipe
     nrfStopListening();
     nrfOpenReadingPipe(pipeIndex, (uint8_t *) pipeName);
     nrfStartListening();
     printf("\n\nReading pipe %d, %s geopend.\n", pipeIndex, pipeName);
-    if(pipeIndex > 1) printf("Onthoud goed dat voor pipes 2 tot 5 alleen het laatste karakter wordt gebruikt. In dit geval is dat %c\n", pipeName[3]);
+    if(pipeIndex > 1) printf("Onthoud goed dat voor pipes 2 tot 5 alleen het laatste karakter wordt gebruikt. In dit geval is dat %c\n", pipeName[4]);
 }
 
 
 void send(char *command) {
     nrfStopListening();
+    // The datasheet says it takes 130 us to switch out of listening mode.
     _delay_us(130);
     uint8_t response = nrfWrite((uint8_t *) command, strlen(command));
-    printf("\n\nVerzonden: %s\nAck ontvangen: %s\n", command, response > 0 ? "JA":"NEE");
-    _delay_ms(5);
+    printf("\nVerzonden: %s\nAck ontvangen: %s\n", command, response > 0 ? "JA":"NEE");
     nrfStartListening();
 }
 
 void help(char *command) {
     printf("\n\nEr zijn 4 commandos:\n*	help\n\tprint deze lijst\n\n*	send <waarde>\n\tverstuurt wat je invoert op waarde naar de geselecteerde pipe\n\n*	wpip <pipenaam>\n\tverander de writing pipe\n");
-	printf("\n*	rpip <index> <pipenaam>\n\tverander de reading pipes. Index is welke van de 6 pipes je wilt aanpassen (0 t/m 5).\n\nHet programma print continu uit wat het ontvangt.\n");
+	printf("\n*	rpip <index> <pipenaam>\n\tverander de reading pipes. Index is welke van de 6 pipes je wilt aanpassen (0 t/m 5).\n\nHet programma print continu uit wat het ontvangt.\n\n");
 }
 
-void chan (char *command) {
+// Function to change the frequency channel.
+void chan(char *command) {
     uint8_t channel = atoi(command);
     nrfStopListening();
     nrfSetChannel(channel);
@@ -190,11 +217,15 @@ ISR(PORTF_INT0_vect) {
 
     PORTF.OUTTGL = PIN0_bm;
     uint8_t packetLength;
-    if(nrfAvailable(NULL)) {                        // is er iets nuttigs binnengekomen??
-        packetLength = nrfGetDynamicPayloadSize();    // kijk hoe groot het is
-        nrfRead(receivedPacket, packetLength);    // lees wat er is gestuurd
-        receivedPacket[packetLength] = '\0';        // zet laatste karakter van de array naar null
-        
+    // Did I receive something actually valuable? 
+    if(nrfAvailable(NULL)) {
+        packetLength = nrfGetDynamicPayloadSize();
+
+        // Put received data into a buffer.
+        nrfRead(receivedPacket, packetLength);
+        receivedPacket[packetLength] = '\0';
+
+        // Relay it to the user.
         printf("Ontvangen: %s\n", receivedPacket);
     }    
 }
