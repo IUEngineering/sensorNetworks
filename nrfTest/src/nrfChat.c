7585a84bc419984faf8d2e902776549403b7b04d
Jochem Leijenhorst <jochemleijenhorst@gmail.com>
2023-10-14 14:25:19 +0200

    Make nrfChat.c and friendList.c more documentable Added some defines and made things more readable.

--- nrfTest/src/nrfChat.c
+++ nrfTest/src/nrfChat.c
@@ -14,13 +14,18 @@
 #define COMMANDS 6
 #define INPUT_BUFFER_LENGTH 38
 
+#define NO_COLOR        "\e[0m"
+#define COMMAND_COLOR   "\e[32m"
+#define ID_COLOR        "\e[35m"
+#define ERROR_COLOR     "\e[31m"
+
 // 'Command line' functions.
-static void help(char *command);
-static void chan(char *command);
-static void send(char *command);
-static void list(char *command);
-static void dest(char *command);
-static void myid(char *command);
+static void help(char *arguments);
+static void chan(char *arguments);
+static void send(char *arguments);
+static void list(char *arguments);
+static void dest(char *arguments);
+static void myid(char *arguments);
 
 static void interpretInput(char *buffer);
 
@@ -35,9 +40,11 @@ static uint8_t destinationId = 0xff;
 void nrfChatInit(void) {
     // Send welcome message
     DEBUG_PRINT("I am a sensor node\n");
-    printf("Welcome to the nrfTester\nMade by Jochem Leijenhorst.\n\nType /help for a list of commands.\n");
     isoInit(messageReceive);
     terminalSetCallback(interpretInput);
+
+    printf("Welcome to the nrfTester\nMade partly by Jochem Leijenhorst.\n\nType " COMMAND_COLOR "/help" NO_COLOR " for a list of commands.\n");
+    printf("Started with ID " ID_COLOR "0x%02x" NO_COLOR " on channel " ID_COLOR "%d" NO_COLOR ".\n", isoGetId(), DEFAULT_CHANNEL);
     
     // Print a nice line (it looks cool).
     for(uint8_t i = 0; i < 64; i++) uartF0_putc('-');
@@ -84,66 +91,76 @@ void interpretInput(char *input) {
     // If a '/' is given, we want to interpret from the character after it.
     input++;
 
-    // Make an array of functions.
-    const void (*comFunc[COMMANDS])(char*) = {
-        send, help, chan, list, dest, myid
+    // Make a corresponding array of 4 letter function names.
+    static const char commands[COMMANDS][4] = {
+        "send", "help", "chan", "list", "dest", "myid"
     };
 
-    // Make a corresponding array of 4 letter function names.
-    const char commands[COMMANDS][4] = {
-        "send", "help", "chan", "list", "dest", "myid"
+    // Make an array of functions.
+    static const void (*functions[COMMANDS])(char*) = {
+        send, help, chan, list, dest, myid
     };
 
     
     for(uint8_t i = 0; i < COMMANDS; i++) {
         if(strncmp(commands[i], input, 4) == 0) {
+            // If the 5th character of the input is already the end, make the 6th character a terminating \0 as well.
+            // This prevents problems with commands that need arguments being run without arguments.
+            if(input[4] == '\0') input[5] = '\0';
+
             // Run the command, with the text after it as argument.
-            comFunc[i](input + 5);
+            functions[i](input + 5);
             return;
         }
     }
     printf("I don't know that command :(\n");
 }
 
-void send(char *command) {
-    if(isoSendPacket(destinationId, (uint8_t*) command, strlen(command))) 
+void send(char *arguments) {
+    if(isoSendPacket(destinationId, (uint8_t*)arguments, strlen(arguments))) 
         printf("Failed to send message\n");    
 }
 
-void help(char *command) {
-    printf("\n\nThere are %d commands:\n\n", COMMANDS);
-    printf("*    \e[32m/help\e[0m\n\tPrint this list.\n\n");
-    printf("*    \e[32m/send\e[0m <message>\n\tSend a message. This can also be done by just typing a message and pressing enter.\n\n");
-    printf("*    \e[32m/chan\e[0m <channel>\n\tChange the channel frequency.\n\n");
-    printf("*    \e[32m/list\e[0m\n\tPrint a list of friends :)\n\n");
-    printf("*    \e[32m/dest\e[0m <id>\n\tChange the id of the destination node.\n\n");
-    printf("*    \e[32m/myid\e[0m\n\tPrints your ID.");
+void help(char *arguments) {
+    printf("There are %d commands:\n\n", COMMANDS);
+    printf("*    " COMMAND_COLOR "/help" NO_COLOR "\n\tPrint this list.\n\n");
+    printf("*    " COMMAND_COLOR "/send" NO_COLOR " <message>\n\tSend a message. This can also be done by just typing a message and pressing enter.\n\n");
+    printf("*    " COMMAND_COLOR "/chan" NO_COLOR " <channel>\n\tChange the channel frequency.\n\n");
+    printf("*    " COMMAND_COLOR "/list" NO_COLOR "\n\tPrint a list of friends :)\n\n");
+    printf("*    " COMMAND_COLOR "/dest" NO_COLOR " <id>\n\tChange the id of the destination node.\n\n");
+    printf("*    " COMMAND_COLOR "/myid" NO_COLOR "\n\tPrints your ID.");
     printf("The program always prints what it is receiving on all open reading pipes.\n\n");
 }
 
 // Function to change the frequency channel.
-void chan(char *command) {
-    uint8_t channel = atoi(command);
+void chan(char *arguments) {
+    uint8_t channel = atoi(arguments);
+
     nrfStopListening();
     nrfSetChannel(channel);
     nrfStartListening();
 
-    printf("\nSwitched to channel %d\n\n", channel);
+    printf("\nSwitched to channel " ID_COLOR "%d" NO_COLOR "\n\n", channel);
 }
 
-void list(char *command) {
+void list(char *arguments) {
     printFriends();
 }
 
-void dest(char *command) {
-    uint8_t newId = strtol(command, NULL, 16);
-    if(newId == 0) printf("Invalid ID entered.\n\n");
+void dest(char *arguments) {
+    if(arguments[0] == '\0') {
+        printf("Usage:" COMMAND_COLOR " /dest" NO_COLOR " <ID>\n\n");
+        return;
+    }
+
+    uint8_t newId = strtol(arguments, NULL, 16);
+    if(newId == 0) printf(ERROR_COLOR "Invalid ID entered.\n\n" NO_COLOR);
     else {
         destinationId = newId;
-        printf("New destination ID is \e[35m0x%02x\e[0m\n\n", newId);
+        printf("New destination ID is " ID_COLOR "0x%02x" NO_COLOR "\n\n", newId);
     }
 }
 
-void myid(char *command) {
-    printf("Your ID is \e[35m0x%02x\e[0m\n\n", isoGetId());
+void myid(char *arguments) {
+    printf("Your ID is " ID_COLOR "0x%02x" NO_COLOR "\n\n", isoGetId());
 }
