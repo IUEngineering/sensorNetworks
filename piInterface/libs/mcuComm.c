4e9d05b78c57ab88b1af29fb774a450cc82f1df1
Jochem Leijenhorst <jochemleijenhorst@gmail.com>
2023-10-24 21:26:02 +0200

    Finish XMega communication On to the friend list storage.

--- piInterface/libs/mcuComm.c
+++ piInterface/libs/mcuComm.c
@@ -10,6 +10,7 @@
 
 #define INPUT_BUFFER_START_SIZE 64
 
+// For receiving from the XMega:
 #define ID_BYTE 0x00
 #define ID_SIZE 2
 
@@ -26,18 +27,31 @@
 #define FRIENDLIST_HEADER_SIZE 2
 #define MAX_FRIENDS 254
 
-#define MAX_INPUT_BUFFER_SIZE   MAX_FRIENDS * BYTES_PER_FRIEND + FRIENDLIST_HEADER_SIZE
+#define ID_PAIR 16
+#define TABLE_HEADER_PAIR 17
+#define HEX_VAL_EVEN_PAIR 18
+#define HEX_VAL_ODD_PAIR  19
+
+// For sending to the XMega:
+#define TRANSMIT_SOMETHING_BYTE 0x01
+
 
 
 void parseFriendsList(uint8_t *data);
-void parsePayload(uint8_t *payload);
-void parseBroadcast(uint8_t *packet);
-void parseRelay(uint8_t *packet);
+void printPacketToWindow(uint8_t *packet, WINDOW *window, const char title[3]);
 
 char* getPrintable(char c);
 
 
-int8_t initInputHandler(void) {
+WINDOW *friendsWindow;
+WINDOW *diagWindow;
+
+// Assumes:
+//- ncurses has been initialised.
+//- `start_color()` has been run.
+//- `fWindow` is an initialized window of at least 34 wide and 4 high.
+//- `dWindow` is an initialized window of exactly 68 wide and at least 2 high.
+int8_t initInputHandler(WINDOW *fWindow, WINDOW *dWindow) {
 
     // Find the XMega by scanning the /dev/ directory for ttyACM(any number)
     DIR *devDir = opendir("/dev");
@@ -60,11 +74,30 @@ int8_t initInputHandler(void) {
     uint8_t inByte = 0;
     uint32_t retryCount = 0;
     do {
-        sleep(0.1);
-        fflush(stdout);
+        mvwprintw(friendsWindow, 0, 0, "Connection retry %d\r", retryCount++);
+        wrefresh(friendsWindow);
         serialPutChar('c');
     }
     while(serialGetChar(&inByte));
+
+
+    init_pair(ID_PAIR, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(TABLE_HEADER_PAIR, COLOR_GREEN, COLOR_BLACK);
+    init_pair(HEX_VAL_EVEN_PAIR, COLOR_BLUE, COLOR_BLACK);  
+    init_pair(HEX_VAL_ODD_PAIR, COLOR_GREEN, COLOR_BLACK); 
+
+    friendsWindow = fWindow;
+    diagWindow = dWindow;
+    scrollok(diagWindow, 1);
+    if(getmaxx(diagWindow) != 68) wprintw(diagWindow, "WARNING: diagnostics window should be exactly 68 wide!");
+
+    mvwprintw(friendsWindow, 0, 0, "My   0 friends:\n");
+    wattrset(friendsWindow, COLOR_PAIR(TABLE_HEADER_PAIR));
+    wprintw(friendsWindow, "ID\tTrust\tActive\tVia\tHops\n");
+    wattrset(friendsWindow, 0);
+
+    wrefresh(friendsWindow);
+
 
     handleNewByte(inByte);
     return 0;
@@ -96,6 +129,7 @@ void handleNewByte(uint8_t newByte) {
     inputBuffer[inputBufferIndex] = newByte;
     inputBufferIndex++;
 
+    fprintf(stderr, "%02x ", newByte);
 
     switch(inputBuffer[0]) {
 
@@ -111,55 +145,101 @@ void handleNewByte(uint8_t newByte) {
             // ↓                       ↓
             if(inputBufferIndex < 2 || inputBufferIndex < FRIENDLIST_HEADER_SIZE + inputBuffer[1] * BYTES_PER_FRIEND)
                 return; //! Notice that this isn't break, it's return.
-                // I only want the code below this switch to run if we just parsed something.
+                // We only want the code below this switch to run if we just parsed something.
 
-            parseFriendsList(inputBuffer);
+            parseFriendsList(inputBuffer + 1);
             break;
 
         case MY_PAYLOAD_BYTE:
             // + 1 because of the header.
             if(inputBufferIndex < PAYLOAD_SIZE + 1) return;
 
-            parsePayload(inputBuffer + 1);
+            printPacketToWindow(inputBuffer + 1, diagWindow, "PL");
             break;
 
         case BROADCAST_BYTE:
             if(inputBufferIndex < PACKET_SIZE + 1) return;
 
-            parseBroadcast(inputBuffer + 1);
+            printPacketToWindow(inputBuffer + 1, diagWindow, "BC");
             break;
 
         case RELAYED_BYTE:
             if(inputBufferIndex < PACKET_SIZE + 1) return;
             
-            parseRelay(inputBuffer + 1);
+            printPacketToWindow(inputBuffer + 1, diagWindow, "RL");
             break;
 
         default: return; 
        
     }
     //* This code only gets run if we just parsed a full message (return vs break in the switch/case).
-    fflush(stdout);
     inputBufferIndex = 0;
+    fprintf(stderr, "\n");
 }
 
 void parseFriendsList(uint8_t *data) {
-  
+
+    // Print the amount of friends in the "My %3d friends:" header.
+    mvwprintw(friendsWindow, 0, 3, "%3d", data[0]);
+    wmove(friendsWindow, 2, 0);
+
+    uint8_t friend = 0;
+    for(; friend < data[0] && friend < getmaxy(friendsWindow) + 1; friend++) {
+
+        // + 1 because of the first byte, which is the amount of friends.
+        const uint16_t index = friend * BYTES_PER_FRIEND + 1;
+
+        wattrset(friendsWindow, COLOR_PAIR(ID_PAIR));
+        wprintw(friendsWindow, "%02x\t", data[index]);      // ID
+        wattrset(friendsWindow, 0);
+
+        wprintw(friendsWindow, "%d\t", data[index + 3]);    // Trust
+        wprintw(friendsWindow, "%d\t", data[index + 4]);    // Active
+
+        wattrset(friendsWindow, COLOR_PAIR(ID_PAIR));
+        wprintw(friendsWindow, "%02x\t", data[index + 2]);  // Via
+        wattrset(friendsWindow, 0);
+
+        wprintw(friendsWindow, "%d\n", data[index + 1]);    // Hops
+    }
+
+    // Clear the rest of the window.
+    wclrtobot(friendsWindow);
+    wrefresh(friendsWindow);
 }
 
-void parsePayload(uint8_t *packet) {
-   
-}
+// Print the packet as a string of hex chars to the given window.
+// Will put the title, if not NULL, at the start of the line.
+void printPacketToWindow(uint8_t *packet, WINDOW *window, const char title[3]) {
+    if(title != NULL) wprintw(window, "%2s: ", title);
+    for(uint8_t i = 0; i < PACKET_SIZE; i += 2) {
+        wattrset(window, COLOR_PAIR(HEX_VAL_EVEN_PAIR));
+        wprintw(window, "%02x", packet[i]);
+        wattrset(window, COLOR_PAIR(HEX_VAL_ODD_PAIR));
+        wprintw(window, "%02x", packet[i + 1]);
+    }
 
-void parseBroadcast(uint8_t *packet) {
+    // Newlines are automatically added by the ncurses scroll functionality.
+
+    // Remove the color :(
+    wattrset(window, 0);
+    if(title != NULL) wprintw(window, "    ");
+
+    for(uint8_t i = 0; i < PACKET_SIZE; i++) {
+        wprintw(window, "%c ", isprint(packet[i]) ? packet[i] : ' ');
+    }
     
+    wrefresh(window);
 }
 
-void parseRelay(uint8_t *packet) {
-   
+void transmitSomething(uint8_t destId) {
+    serialPutChar(TRANSMIT_SOMETHING_BYTE);
+    serialPutChar(destId);
+    wprintw(diagWindow, "Sent something to %02x\n", destId);
 }
 
-char* getPrintable(char c) {
+
+char *getPrintable(char c) {
 
     switch(c) {
         case '\n':
--- piInterface/libs/mcuComm.h
+++ piInterface/libs/mcuComm.h
@@ -2,8 +2,10 @@
 #define _INPUTHANDLER_H_
 
 #include <stdint.h>
+#include <ncurses.h>
 
-void initInputHandler(void);
+int8_t initInputHandler(WINDOW *friendsWin, WINDOW *diagnosticsWindow);
 void handleNewByte(uint8_t newByte);
+void transmitSomething(uint8_t destId);
 
 #endif // _INPUTHANDLER_H_
